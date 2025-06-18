7aeb2c6097606a163d91998053740f1aaf3fb401
Emrecan35 <2204109052@std.idu.edu.tr>
2025-06-18 17:58:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,21 @@ def main():
 
     if st.button("Tahmin Et"):
         prediction = model.predict(input_scaled)
+            if st.button("Tahmin Et"):
+        prediction = model.predict(input_scaled)
+
+        # ✅ Olasılık tahmini burada:
+        prob = model.predict_proba(input_scaled)[0][1]
+        st.write(f"💡 İçilebilir olasılığı: {prob:.3f}")
+
+        result = "İÇİLEBİLİR SU 💧" if prediction[0] == 1 else "İÇİLEMEZ SU ❌"
+
+        if prediction[0] == 1:
+            st.success(f"Tahmin Sonucu: {result}")
+        else:
+            st.error(f"Tahmin Sonucu: {result}")
+
+        
         result = "İÇİLEBİLİR SU 💧" if prediction[0] == 1 else "İÇİLEMEZ SU ❌"
 
         if prediction[0] == 1:
